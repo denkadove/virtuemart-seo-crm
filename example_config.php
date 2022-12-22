44038b027fb7540e6a53136b0ecdb40d3711e04f
Denis Sunflower <denkadove@gmail.com>
2022-12-22 05:42:47 +0300

    Update example_config.php

--- example_config.php
+++ example_config.php
@@ -2,13 +2,14 @@
     namespace Core;
 
     class crmConfig {
-        public $user = 'root';
-        public $pass = '';
-        public $host = 'localhost';
-        public $dbname = 'domain.ru';
-        public $table_prefix  = 'g82ax_';
-        public $tax_custom_field_id = '27';
-        public $availability_custom_field_id = '28';
-        public $canon_canegory_id = '197';
+        public string $user = 'root';
+        public string $pass = '';
+        public string $host = 'localhost';
+        public string $dbname = 'domain.ru';
+        public string $table_prefix  = 'g82ax_';
+        public string $tax_custom_field_id = '27';
+        public string $availability_custom_field_id = '28';
+        public string $canon_canegory_id = '197';
+        public string $metatag_product_list = '168,207';
     }
 
