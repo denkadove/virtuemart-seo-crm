e142822a155d9248ef6f22740bed73b3a1383cdd
Denis Sunflower <denkadove@gmail.com>
2022-12-28 01:32:49 +0300

    some code rewrite

--- views/templates/default/index.php
+++ views/templates/default/index.php
@@ -8,6 +8,3 @@
     require_once('views/templates/default/blocks/header.php');
     require_once('views/templates/default/blocks/main_content.php');
     require_once('views/templates/default/blocks/footer.php');
-?>
-
-    
